c05fa170576655719ed2a39ef573298018950209
hellohawaii <liushuzhi16@mails.ucas.ac.cn>
2021-03-19 21:52:19 +0800

    Reconnect when no more data recieved
    TODO:
    1. Strangely, the old recieved data is not written to the file when
       restart the socket. Change the mode the file is opened from 'wb' to
       'ab' fixed this problem. However, I still don't know why the recieved
       data is abandoned.
    2. Currently, can not detect the problems that the socket only recieve audio/video data. This also need us to restart the socket.

--- recorder.py
+++ recorder.py
@@ -76,73 +76,81 @@ def pack_adts_frame(data):
 
 
 if __name__ == '__main__':
-    # Initialize socket
-    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    s.connect((SERVER_IP, SERVER_PORT))
-    print ('Connected to server')
+    recieve_no_data = True
+    first_connect = True
+    video_fps = [open('vstream{}.h264'.format(i), 'ab', buffering=0) for i in range(2)]
+    audio_fp = open('astream.aac', 'ab', buffering=0)
+    while recieve_no_data:
+        # Initialize socket
+        s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        s.connect((SERVER_IP, SERVER_PORT))
+        print ('Connected to server')
 
-    # Login
-    user_info = b'reach' + b'\x00' * 15 + b'reachplayer' + b'\x00' * 13
-    send_msg(s, MSG_TYPE_LOGIN, user_info)
-    _ = recv_msg(s)
-    print ('Logged in')
+        # Login
+        user_info = b'reach' + b'\x00' * 15 + b'reachplayer' + b'\x00' * 13
+        send_msg(s, MSG_TYPE_LOGIN, user_info)
+        _ = recv_msg(s)
+        print ('Logged in')
 
-    # Select channel
-    channel_id = int(sys.argv[1])
-    channel_info = struct.pack('i', channel_id)
-    send_msg(s, MSG_TYPE_DATAREQ, channel_info)
-    _ = recv_msg(s)
-    print ('Opened channel')
+        # Select channel
+        channel_id = int(sys.argv[1])
+        channel_info = struct.pack('i', channel_id)
+        send_msg(s, MSG_TYPE_DATAREQ, channel_info)
+        _ = recv_msg(s)
+        print ('Opened channel')
 
-    video_fps = [open('vstream{}.h264'.format(i), 'wb') for i in range(2)]
-    audio_fp = open('astream.aac', 'wb')
-    # Receive data packets
+        if first_connect:
+            n_vid_packets = 0
+            video_ready = [False for _ in range(2)]
+            audio_ready = False
+            # NOTE: ticks may not always be available (e.g. 0),
+            # so we use packet length to differentiate frames
+            prev_flen = -1
+            avg_fps = -1
+            first_connect = False
+        # Receive data packets
+        try:
+            screen1_time_before = time.time()
+            screen2_time_before = time.time()
+            audio_time_before = time.time()
+            while True:
+                # if time.time() > 1600135860: break
+                data = recv_msg(s)
+                if data is None:
+                    break
+                # Incoming HDB frame
+                if len(data) >= 48:
+                    res = parse_hdb_frame(data)
+                    if res['ret'] != 0:
+                        continue
+                    if res['header']['codec'] == 'H264':
+                        screen_idx = res['header']['idx']
+                        if screen_idx == 0:
+                            flen = res['header']['flen']
+                            if flen != prev_flen:
+                                n_vid_packets += 1
+                                prev_flen = flen
+                                fps = res['header']['fps']
+                                if avg_fps < 0: avg_fps = fps
+                                else: avg_fps = 0.95 * avg_fps + 0.05 * fps
+                                print ('Progress: {} frames @ {} fps, avg {:.1f} fps, ~{:.2f} sec'.format(
+                                    n_vid_packets, fps, avg_fps, n_vid_packets / avg_fps))
+                        if not video_ready[screen_idx] and res['header']['seg'] == 2:
+                            video_ready[screen_idx] = True
+                        if video_ready[screen_idx]:
+                            video_fps[screen_idx].write(res['data'])
+                    elif res['header']['codec'] == 'ADTS':
+                        if not audio_ready and res['header']['seg'] >= 2 and video_ready[0]:
+                            audio_ready = True
+                        if audio_ready:
+                            audio_fp.write(pack_adts_frame(res['data']))
 
-    n_vid_packets = 0
-    video_ready = [False for _ in range(2)]
-    audio_ready = False
-    # NOTE: ticks may not always be available (e.g. 0),
-    # so we use packet length to differentiate frames
-    prev_flen = -1
-    avg_fps = -1
+        except KeyboardInterrupt:
+            recieve_no_data = False
+            print ('Interrupted!')
 
-    try:
-        while True:
-            # if time.time() > 1600135860: break
-            data = recv_msg(s)
-            if data is None:
-                print ('No data...')
-                break
-            # Incoming HDB frame
-            if len(data) >= 48:
-                res = parse_hdb_frame(data)
-                if res['ret'] != 0:
-                    continue
-                if res['header']['codec'] == 'H264':
-                    screen_idx = res['header']['idx']
-                    if screen_idx == 0:
-                        flen = res['header']['flen']
-                        if flen != prev_flen:
-                            n_vid_packets += 1
-                            prev_flen = flen
-                            fps = res['header']['fps']
-                            if avg_fps < 0: avg_fps = fps
-                            else: avg_fps = 0.95 * avg_fps + 0.05 * fps
-                            print ('Progress: {} frames @ {} fps, avg {:.1f} fps, ~{:.2f} sec'.format(
-                                n_vid_packets, fps, avg_fps, n_vid_packets / avg_fps))
-                    if not video_ready[screen_idx] and res['header']['seg'] == 2:
-                        video_ready[screen_idx] = True
-                    if video_ready[screen_idx]:
-                        video_fps[screen_idx].write(res['data'])
-                elif res['header']['codec'] == 'ADTS':
-                    if not audio_ready and res['header']['seg'] >= 2 and video_ready[0]:
-                        audio_ready = True
-                    if audio_ready:
-                        audio_fp.write(pack_adts_frame(res['data']))
-    except KeyboardInterrupt:
-        print ('Interrupted!')
+        s.close()
 
-    s.close()
     for video_fp in video_fps:
         video_fp.close()
     audio_fp.close()
